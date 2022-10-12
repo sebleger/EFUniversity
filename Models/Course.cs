6b6c798cc104d559e811bfbcf85fd0d5622c4b81
Sébastien <sleger003@ensc.fr>
2022-10-12 10:10:58 +0200

    Ajout modèles

---
+++ Models/Course.cs
@@ -0,0 +1,7 @@
+public class Course
+{
+    public int Id{get;set;}
+    public string Title{get;set;}
+    public int Credits{get;set;}
+    public ICollection<Enrollment> Enrollments{get;set;}
+}
---
+++ Models/Enrollment.cs
@@ -0,0 +1,9 @@
+public class Enrollment
+{
+    public int Id{get;set;}
+    public Grade Grade{get;set;}
+    public int StudentId{get;set;}
+    public int CourseId{get;set;}
+    public Student Student{get;set;}
+    public Course Course{get;set;}
+}
